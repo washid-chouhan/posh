0152dcee7813d57c9200d67b2a4ee09ed2b30050
veedel <hot.sa.chhora@gmail.com>
2025-07-15 15:49:01 +0530

    second

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5f8d65b8893980cb"; }
+/******/ 		__webpack_require__.h = function() { return "cea6fbe827f55f27"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- screens/EntryScreen/SignIn/index.tsx
+++ screens/EntryScreen/SignIn/index.tsx
@@ -72,7 +72,7 @@ const SignIn = ({ onClick, onResetPassword }: SignInProps) => {
       </div>
       <div className={styles.btns}>
         <button onClick={verifyOtp} className={cn("button", styles.button)}>
-          Sign in
+          Sign inn
         </button>
         <Link className={cn("button", styles.button)} href="/">
           <Image src="/images/google.svg" width={24} height={24} alt="" />
